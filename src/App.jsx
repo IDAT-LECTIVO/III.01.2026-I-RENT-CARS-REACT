af98e0fecbd41510146192db5e88899b33c4799a
Jhefferson <jheffersonjhgg@gmail.com>
2026-03-19 10:48:02 -0500

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import "./App.css";
 import { Wellcome } from "./components/home/Wellcome";
 import { Characteristics } from "./components/details/Characteristics";
 import { Login } from "./pages/Login";
+import { Route, Routes } from "react-router-dom";
+import { Register } from "./pages/Register";
+import { Page404 } from "./pages/Page404";
 
 function App() {
   return(
-    <>
-      <Login/>
-    </>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="*" element={<Page404 />} />
+    </Routes>
   )
 }
 
---
+++ src/pages/Page404.jsx
@@ -0,0 +1,5 @@
+export function Page404() {
+  return(
+    <h1>PAGINA NO ENCONTRADA <a href="/">Volver a la página principal</a></h1>
+  )
+}
